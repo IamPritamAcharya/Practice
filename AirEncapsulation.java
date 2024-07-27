242a4d45cbb5d44e7555a74703a713779986d570
IamPritamAcharya <pritamach.exe@gmail.com>
2024-07-27 10:54:21 +0530

    Add AirEncapsulation.java

---
+++ AirEncapsulation.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class Main {
+
+    static int air_bet_3(int x,int y, int z) {
+        int min = (x < z) ? x : z;
+
+        if (min > y) {
+            return min - y;
+        }
+
+        return 0;
+    }
+
+    static void totalAirEnc(int[] arr) {
+        int ans = 0;
+
+        for (int i = 0; i < arr.length-2; i++) {
+            ans += air_bet_3(arr[i], arr[i+1], arr[i+2]);          
+        }
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        totalAirEnc(arr);
+    }
+}
+
