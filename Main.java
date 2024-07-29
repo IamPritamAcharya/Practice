bc1fb06f6720a26d47295b4f71408e30b166a802
IamPritamAcharya <pritamach.exe@gmail.com>
2024-07-29 16:55:47 +0530

    Add squarePatch function implementation in Java

--- Main.java
+++ Main.java
@@ -1,35 +1,56 @@
+/*
+Create a function that takes an integer and outputs an n x n square solely consisting of the integer n.
+
+Examples
+squarePatch(3) ➞ [
+  [3, 3, 3],
+  [3, 3, 3],
+  [3, 3, 3]
+]
+
+squarePatch(5) ➞ [
+  [5, 5, 5, 5, 5],
+  [5, 5, 5, 5, 5],
+  [5, 5, 5, 5, 5],
+  [5, 5, 5, 5, 5],
+  [5, 5, 5, 5, 5]
+]
+
+squarePatch(1) ➞ [
+  [1]
+]
+
+squarePatch(0) ➞ []
+*/
+
 import java.util.*;
-import java.util.Scanner;
 
 public class Main {
 
-    static int air_bet_3(int x,int y, int z) {
-        int min = (x < z) ? x : z;
-
-        if (min > y) {
-            return min - y;
+	public static int[][] squarePatch(int n) {
+        int[][] ans = new int[n][n];
+		for (int i = 0; i < ans.length; i++) {
+            for (int j = 0; j < ans.length; j++) {
+                ans[i][j] = n;
+            }
         }
-
-        return 0;
-    }
-
-    static void totalAirEnc(int[] arr) {
-        int ans = 0;
-
-        for (int i = 0; i < arr.length-2; i++) {
-            ans += air_bet_3(arr[i], arr[i+1], arr[i+2]);          
-        }
-        System.out.println(ans);
-    }
+        return ans;
+	}
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int arr[] = new int[n];
+        int n = 3;
+
+        int[][] ans = squarePatch(n);
+
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            for (int j = 0; j < n; j++) {
+                System.out.print(ans[i][j] + ", ");
+            }
+            System.out.println();
         }
-        totalAirEnc(arr);
+
     }
 }
+
 
