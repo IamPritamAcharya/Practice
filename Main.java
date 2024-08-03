6c0898e8a97c2551858cdfd6fc35bbdc316ce9b1
IamPritamAcharya <pritamach.exe@gmail.com>
2024-08-03 18:37:09 +0530

    Add main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 /*
- * fibonacci
+Given a number n. Count the number of digits in n which evenly divide n. Return an integer, total number of digits of n which divides n evenly.
+
+Note :- Evenly divides means whether n is divisible by a digit i.e. leaves a remainder 0 when divided.
+ 
+
+Examples :
+
+Input: n = 12
+Output: 2
+Explanation: 1, 2 when both divide 12 leaves remainder 0.
+Input: n = 2446
+Output: 1
+Explanation: Here among 2, 4, 6 only 2 divides 2446 evenly while 4 and 6 do not.
  */
 
 import java.util.*;
@@ -7,27 +19,24 @@ import java.util.*;
 
 public class Main {
 
-    static int getNthFibo(int n) {
-        int first = 1;
-        int second = 1;
-        int next = 2;
-        if (n == 1 || n == 2) {
-            return 1;
+    static int evenlyDivides(int n){
+        
+        int count = 0;
+        int num = n;
+        while (n>0) {
+            int r = n % 10;
+            if ((r>=1) && (num%r == 0)) {
+                count++;
+            }
+            n/=10;
         }
-        for (int i = 0; i < n-2; i++) {
-            next = first + second;
-            first = second;
-            second = next;
-        }
-        return next;
+        return count;
     }
-
-
 
    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        System.out.println(getNthFibo(num));
+        System.out.println(evenlyDivides(num));
     }
 }
 
