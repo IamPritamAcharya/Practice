e5c0e72626a37e4bd95c5b22149630c46d1612cc
IamPritamAcharya <pritamach.exe@gmail.com>
2024-08-18 10:08:45 +0530

    1920

---
+++ leetcode1920.java
@@ -0,0 +1,15 @@
+public class leetcode1920 {
+    public static int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
