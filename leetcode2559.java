889e7453acf053ec704ea31f1565c1357488d3fe
IamPritamAcharya <pritamach.exe@gmail.com>
2025-01-02 22:29:52 +0530

    prefix sum vowel count preoblem

---
+++ leetcode2559.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class leetcode2559 {
+    public static int[] vowelStrings(String[] words, int[][] queries) {
+        int n = words.length;
+        int[] prefixSum = new int[n + 1]; 
+
+        for (int i = 0; i < n; i++) {
+            if (isVowel(words[i].charAt(0)) && isVowel(words[i].charAt(words[i].length() - 1))) {
+                prefixSum[i + 1] = prefixSum[i] + 1;
+            } else {
+                prefixSum[i + 1] = prefixSum[i];
+            }
+        }
+
+        int[] ans = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int left = queries[i][0];
+            int right = queries[i][1];
+            ans[i] = prefixSum[right + 1] - prefixSum[left]; 
+        }
+
+        return ans;
+    }
+
+    public static boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"aba", "bcb", "ece", "aa", "e"};
+        int[][] queries = {{0, 2}, {1, 4}, {1, 1}};
+        System.out.println(Arrays.toString(vowelStrings(words, queries)));
+    }
+}
