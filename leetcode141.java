b48d37237f54e27e84cfd558a7d436f20e8b6303
IamPritamAcharya <pritamach.exe@gmail.com>
2025-01-07 20:27:58 +0530

    tortoise heir algo

---
+++ leetcode141.java
@@ -0,0 +1,52 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class leetcode141 {
+
+    public static ListNode convert_Array_to_Linkedlist(int[] arr) {
+        ListNode head =  new ListNode(arr[0]);
+        ListNode mover = head;
+
+        for (int i = 1; i < arr.length; i++) {
+            ListNode temp = new ListNode(arr[i]);
+            mover.next = temp; 
+            mover = temp;
+        }
+
+        return head;
+    }
+    static void print_Linkedlist(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println();
+        
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            if (fast == slow) {
+                return true;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return false;
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
