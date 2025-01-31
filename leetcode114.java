9664d74b60a4842a35e02ea8ae583daf961e258d
IamPritamAcharya <pritamach.exe@gmail.com>
2025-02-01 01:56:16 +0530

    Binary Tree - END _Flatter

---
+++ leetcode114.java
@@ -0,0 +1,20 @@
+public class leetcode114 {
+    TreeNode prev = null;
+
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        flatten(root.right);
+        flatten(root.left);
+
+        root.right = prev;
+        root.left = null;
+        prev = root;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
