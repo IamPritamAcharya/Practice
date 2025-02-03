700773fc832205bc39e6f53ccc82410b5f6da5f3
IamPritamAcharya <pritamach.exe@gmail.com>
2025-02-03 09:19:26 +0530

    bfs tree

---
+++ leetcode107.java
@@ -0,0 +1,53 @@
+
+import java.util.ArrayList;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class leetcode107 {
+    public static List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        if (root == null) {
+            return ans;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> temp = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+
+                temp.add(curr.val);
+
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.add(curr.right);
+                }
+
+            }
+
+            ans.add(0, temp);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        System.out.println(levelOrderBottom(root));
+
+    }
+}
