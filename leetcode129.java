e0edccbf182d8c3a43f17854f8a27e9d288804a7
IamPritamAcharya <pritamach.exe@gmail.com>
2025-02-03 09:49:02 +0530

    SUM OF ROOT TO LElAF NODES

---
+++ leetcode129.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class leetcode129 {
+    public static int sumNumbers(TreeNode root) {
+        return helper(root, 0);
+        
+    }
+
+    static int helper(TreeNode root, int currSum) {
+        if (root == null) {
+            return 0;
+        }
+
+        currSum = currSum*10 + root.val;
+
+        if (root.left == null && root.right == null) {
+            return currSum;
+        }
+
+        return helper(root.left, currSum) + helper(root.right, currSum);
+    }
+
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(1);
+        node.left = new TreeNode(2);
+        node.right = new TreeNode(3);
+        node.left.left = new TreeNode(5);
+        node.left.right = new TreeNode(1);
+
+        sumNumbers(node);
+
+
+    }
+}
