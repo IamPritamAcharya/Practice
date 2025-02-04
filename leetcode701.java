ceea4df9d3498d36bf5dfe3194daea6ba5fa5546
IamPritamAcharya <pritamach.exe@gmail.com>
2025-02-04 10:11:02 +0530

    BST Insert

---
+++ leetcode701.java
@@ -0,0 +1,43 @@
+public class leetcode701 {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        } else {
+            root.left = insertIntoBST(root.left, val);
+        }
+
+        return root;
+    }
+
+    // public TreeNode insertIntoBST_iterative(TreeNode root, int val) {
+    //     if (root == null) {
+    //         return new TreeNode(val);
+    //     }
+
+    //     TreeNode curr = root;
+
+    //     while(true) {
+    //         if (curr.val <= val) {
+    //             if (curr.right != null) {
+    //                 curr = curr.right;
+    //             } else {
+    //                 curr.right = new TreeNode(val);
+    //                 break;
+    //             }
+    //         } else {
+    //             if (curr.left != null) {
+    //                 curr = curr.left;
+    //             } else {
+    //                 curr.left = new TreeNode(val);
+    //                 break;
+    //             }
+    //         }
+    //     }
+
+    //     return root;
+    // }
+}
